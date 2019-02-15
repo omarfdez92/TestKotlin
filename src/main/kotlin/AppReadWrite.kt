1be488f87559d2f7b1d9b8f7f2d39d3b45e6e253
Omar Parra <omarfdez92@gmail.com>
2019-02-15 17:18:26 -0600

    App example

--- src/main/kotlin/AppReadWrite.kt
+++ src/main/kotlin/AppReadWrite.kt
@@ -1,1 +1,85 @@
+import java.io.FileReader
+import java.io.FileWriter
+
 /** Created by Omar Parra on 2/15/19 at 4:47 PM */
+
+fun main(args: Array<String>) {
+
+    menu()
+
+}
+
+fun menu(){
+
+    do {
+
+        println("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-")
+        println("Enter: \n1. Write\n2. Read\n3. Exit\n")
+        var choice = readLine()!!.toInt()
+
+        when (choice){
+            1 -> {
+                println("Write something: ")
+                var message = readLine()!!.toString()
+                if (message.equals("")){
+                    println("Please add something!!!")
+                }else {
+                    writetoFile(message)
+                }
+                style()
+            }
+            2 -> {
+                println("Hello in texting.txt file you have: ")
+                readfromFile()
+                style()
+            }
+            else -> ("This option is not valid")
+        }
+
+    }while(choice != 3)
+
+    println("Thank you, next!... I mean bye!")
+    style()
+
+}
+
+fun writetoFile(message: String){
+
+    try {
+
+        var writer = FileWriter("texting.txt", true)
+        writer.write(message + " \n")
+        writer.close()
+
+    }catch (e: Exception){
+        println("EXC: $e")
+    }
+
+}
+
+fun readfromFile(){
+
+    try {
+
+        var reader = FileReader("texting.txt")
+        var char: Int?
+
+        do {
+            char = reader.read()
+            print(char.toChar())
+
+        }while (char != -1)
+
+    }catch (e: Exception){
+        println("Exc: ${e.message}")
+    }
+
+}
+
+fun style(){
+    Thread.sleep(700)
+    println("Done/EOF")
+    println("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-")
+}
+
+
